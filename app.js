d8b20072a22ab3efd202d856421ac81ede3e1b15
Ryan <mills.ryan.011@gmail.com>
2023-01-15 15:51:54 -0500

    default params & spread example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+function rollDie(numSides) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+Math.max(13,4,5,6,7,8,91,199,1233,145599);
+// expecting many different arguments
+
+const nums = [13,4,5,6,7,8,91,199,1233,145599];
+
+Math.max(...nums); // === Math.max(13,4,5,6,7,8,91,199,1233,145599)
